cb25c26688f2d09cc3ea85a91fb9eb9768c8f7a4
Przemysław Zalewski <przemekz689@gmail.com>
2020-07-20 18:09:51 +0200

    Added class condition

--- src/main/java/pl/zemoz/qcmmocoreintegration/MMOCoreIntegration.java
+++ src/main/java/pl/zemoz/qcmmocoreintegration/MMOCoreIntegration.java
@@ -4,14 +4,18 @@ import com.guillaumevdn.gcore.lib.integration.PluginIntegration;
 import com.guillaumevdn.questcreator.module.condition.ConditionType;
 import com.guillaumevdn.questcreator.module.object.QObjectType;
 import com.guillaumevdn.questcreator.util.parseable.container.CPLocation;
+import pl.zemoz.qcmmocoreintegration.condition.MMOCoreClass;
 import pl.zemoz.qcmmocoreintegration.condition.MMOCoreMainLevel;
 import pl.zemoz.qcmmocoreintegration.condition.MMOCoreProfessionLevel;
 import pl.zemoz.qcmmocoreintegration.object.ServerMMOCoreGiveMainXP;
 import pl.zemoz.qcmmocoreintegration.object.ServerMMOCoreGiveProfessionXP;
 
 public class MMOCoreIntegration extends PluginIntegration {
+
+    public static ConditionType MMOCORE_CLASS = null;
     public static ConditionType MMOCORE_MAIN_LEVEL = null;
     public static ConditionType MMOCORE_PROFESSION_LEVEL = null;
+
     public static QObjectType SERVER_MMOCORE_GIVE_MAIN_XP = null;
     public static QObjectType SERVER_MMOCORE_GIVE_PROFESSION_XP = null;
 
@@ -20,15 +24,19 @@ public class MMOCoreIntegration extends PluginIntegration {
     }
 
     public void enable() {
+        MMOCORE_CLASS = ConditionType.registerType("MMOCORE_CLASS", MMOCoreClass.class, "MMOCore");
         MMOCORE_MAIN_LEVEL = ConditionType.registerType("MMOCORE_MAIN_LEVEL", MMOCoreMainLevel.class, "MMOCore");
         MMOCORE_PROFESSION_LEVEL = ConditionType.registerType("MMOCORE_PROFESSION_LEVEL", MMOCoreProfessionLevel.class, "MMOCore");
+
         SERVER_MMOCORE_GIVE_MAIN_XP = QObjectType.registerType("SERVER_MMOCORE_GIVE_MAIN_XP", ServerMMOCoreGiveMainXP.class, QObjectType.Category.SERVER, CPLocation.Need.USELESS, "MMOCore");
         SERVER_MMOCORE_GIVE_PROFESSION_XP = QObjectType.registerType("SERVER_MMOCORE_GIVE_PROFESSION_XP", ServerMMOCoreGiveProfessionXP.class, QObjectType.Category.SERVER, CPLocation.Need.USELESS, "MMOCore");
     }
 
     public void disable() {
+        MMOCORE_CLASS = MMOCORE_CLASS.unregister();
         MMOCORE_MAIN_LEVEL = MMOCORE_MAIN_LEVEL.unregister();
         MMOCORE_PROFESSION_LEVEL = MMOCORE_PROFESSION_LEVEL.unregister();
+
         SERVER_MMOCORE_GIVE_MAIN_XP = SERVER_MMOCORE_GIVE_MAIN_XP.unregister();
         SERVER_MMOCORE_GIVE_PROFESSION_XP = SERVER_MMOCORE_GIVE_PROFESSION_XP.unregister();
     }
---
+++ src/main/java/pl/zemoz/qcmmocoreintegration/condition/MMOCoreClass.java
@@ -0,0 +1,57 @@
+package pl.zemoz.qcmmocoreintegration.condition;
+
+import com.guillaumevdn.gcore.lib.material.Mat;
+import com.guillaumevdn.gcore.lib.parseable.Parseable;
+import com.guillaumevdn.gcore.lib.parseable.editor.EditorGUI;
+import com.guillaumevdn.gcore.lib.parseable.primitive.PPEnum;
+import com.guillaumevdn.gcore.lib.parseable.primitive.PPString;
+import com.guillaumevdn.questcreator.QCLocaleEditor;
+import com.guillaumevdn.questcreator.module.condition.Condition;
+import com.guillaumevdn.questcreator.module.quest.Quest;
+import net.Indyuce.mmocore.api.player.PlayerData;
+import org.bukkit.entity.Player;
+import pl.zemoz.qcmmocoreintegration.MMOCoreIntegration;
+
+import java.util.List;
+
+public class MMOCoreClass extends Condition {
+
+    private final PPEnum<Operation> operation = addComponent(new PPEnum<>("operation", this, "EQUALS", Operation.class, "operation", true, 10, EditorGUI.ICON_ENUM, QCLocaleEditor.GUI_QUESTCREATOR_EDITOR_GENERIC_OPERATIONLORE.getLines()));
+
+    private final PPString className = addComponent(new PPString("class", this, "warrior", true, 12, EditorGUI.ICON_STRING, QCLocaleEditor.GUI_QUESTCREATOR_EDITOR_GENERIC_SETTINGLORE.getLines()));
+
+    public MMOCoreClass(String id, Parseable parent, boolean mandatory, int editorSlot, Mat editorIcon, List<String> editorDescription) {
+        super(id, MMOCoreIntegration.MMOCORE_CLASS, parent, mandatory, editorSlot, editorIcon, editorDescription);
+    }
+
+    public Operation getOperation(Player parser) {
+        return this.operation.getParsedValue(parser);
+    }
+
+    public String getClassName(Player parser) {
+        return this.className.getParsedValue(parser);
+    }
+
+    public boolean isValid(Player player, Player parser, Quest quest) {
+        Operation operation = getOperation(parser);
+        String className = getClassName(parser);
+
+        PlayerData playerData = PlayerData.get(player);
+
+        switch (operation) {
+            case EQUALS:
+                return playerData.getProfess().getName().equals(className);
+            case DIFFERENT:
+                return !playerData.getProfess().getName().equals(className);
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+
+    public void take(Player player, Player parser, Quest quest) {
+    }
+
+    public enum Operation {
+        EQUALS, DIFFERENT
+    }
+}
